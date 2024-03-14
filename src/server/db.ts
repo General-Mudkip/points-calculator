d851aa52d18f5eecfcc34589b935837fc6c997dc
General-Mudkip <bencethegreat@gmail.com>
2024-03-14 23:33:06 +0000

    Updated DB stuff

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,24 +1,29 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaLibSQL } from "@prisma/adapter-libsql"
 import { createClient } from "@libsql/client"
+import { PrismaLibSQL } from "@prisma/adapter-libsql"
+import { PrismaClient } from "@prisma/client"
 
 import { env } from "~/env"
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
-}
-
 const libsql = createClient({
-    url: `${process.env.TURSO_DATABASE_URL}`,
-    authToken: `${process.env.TURSO_AUTH_TOKEN}`
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN
 })
 
 const adapter = new PrismaLibSQL(libsql)
 
-export const db =
-    globalForPrisma.prisma ??
+const createPrismaClient = () =>
     new PrismaClient({
-        adapter
+        adapter: adapter,
+        log:
+            env.NODE_ENV === "development"
+                ? ["query", "error", "warn"]
+                : ["error"]
     })
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: ReturnType<typeof createPrismaClient> | undefined
+}
+
+export const db = globalForPrisma.prisma ?? createPrismaClient()
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db
