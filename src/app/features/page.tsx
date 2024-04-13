172384f7aea9a956e51b0ec31f212f5d90574aa1
General-Mudkip <bencethegreat@gmail.com>
2024-04-13 12:57:02 +0100

    Made features page responsive

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/homepage/navbar"
 
 import thirdLevelImage from "public/thirdLevel.jpg"
 import analyticsImage from "public/analytics.jpg"
+import totalPointsImage from "public/totalPoints.jpg"
 
 const imageStyle = {
     scale: "100%"
@@ -15,15 +16,15 @@ const Features = () => {
             <Navbar />
 
             <div>
-                <div className="mx-4 flex flex-col items-center">
+                <div className="flex flex-col items-center">
                     <h1 className="text-center text-6xl font-semibold underline decoration-sky-500 lg:text-8xl">
                         Features
                     </h1>
-                    <span className="sm:48 mt-6 text-center text-lg text-gray-800 lg:w-[48rem] lg:text-xl">
+                    <span className="mt-6 px-4 text-center text-lg text-gray-800 lg:w-[48rem] lg:text-xl">
                         BetterExams Points comes with many features that aim to make your experience as worry-free and amazing as possible.
                     </span>
-                    <div className="mt-14 flex w-screen flex-row justify-around px-20">
-                        <div className="flex w-[50rem] flex-col p-16">
+                    <div className="mt-24 flex w-screen flex-col justify-center px-4 text-center lg:mt-14 lg:flex-row lg:justify-around lg:px-20">
+                        <div className="flex flex-col lg:w-[50rem] lg:p-16">
                             <h1 className="text-4xl font-semibold">Extensive Statistics</h1>
                             <p className="mt-2 text-lg">
                                 Log and analyse various key figures. BetterExams Points automatically calculates your total points, both over time, and
@@ -31,7 +32,7 @@ const Features = () => {
 
                             </p>
                         </div>
-                        <div className="rounded-md shadow-xl">
+                        <div className="mt-8 rounded-md shadow-xl lg:mt-0">
                             <Image
                                 src={analyticsImage}
                                 priority={true}
@@ -42,23 +43,52 @@ const Features = () => {
                             />
                         </div>
                     </div>
-                    <div className="my-14 flex w-screen flex-row justify-around px-20">
-                        <div className="rounded-md shadow-xl">
+
+                    <div className="mt-20 flex w-screen flex-col justify-center px-4 text-center lg:mt-14 lg:flex-row lg:justify-around lg:px-20">
+                        <div className="mt-8 hidden rounded-md shadow-xl lg:mt-0 lg:block">
                             <Image
                                 src={thirdLevelImage}
                                 priority={true}
                                 placeholder="blur"
                                 width="800"
                                 style={imageStyle}
-                                alt="Picture of the dashboard home page"
+                                alt="Picture of a third level prediction based on user points"
                             />
                         </div>
-                        <div className="flex w-[50rem] flex-col p-16">
+                        <div className="flex flex-col lg:w-[50rem] lg:p-16">
+                            <h1 className="text-4xl font-semibold">Aim For The Sky</h1>
+                            <p className="mt-2 text-lg">
+                                Easily set points goals and see how close you are to achieving the required points for your chosen CAO course.
+                            </p>
+                        </div>
+                        <div className="mt-8 rounded-md shadow-xl lg:mt-0 lg:hidden">
+                            <Image
+                                src={thirdLevelImage}
+                                priority={true}
+                                placeholder="blur"
+                                width="800"
+                                style={imageStyle}
+                                alt="Picture of a third level prediction based on user points"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="my-20 flex w-screen flex-col justify-center px-4 text-center lg:mt-14 lg:flex-row lg:justify-around lg:px-20">
+                        <div className="flex flex-col lg:w-[50rem] lg:p-16">
                             <h1 className="text-4xl font-semibold">Track Your Total Points</h1>
                             <p className="mt-2 text-lg">
-                                Easily see how close you are to achieving the required points for your chosen CAO points. Set goals and track
-                                your progress over time.
+                                Visualise your total points value, based on your average test result for each subject.
                             </p>
+                        </div>
+                        <div className="mt-8 rounded-md shadow-xl lg:mt-0">
+                            <Image
+                                src={totalPointsImage}
+                                priority={true}
+                                placeholder="blur"
+                                width="800"
+                                style={imageStyle}
+                                alt="Picture of the total points over time chart"
+                            />
                         </div>
                     </div>
                 </div>
